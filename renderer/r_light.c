m-x-d/KMQuake2

commit 399aab28d394860a6dc1ca3c16676808bdc6468b

Added lightmap scaling (adapted from QSS, https://github.com/Shpoike/Quakespasm/commit/2fc55c878dd74a627d3a35c955c3cd501601d82a).

renderer/r_light.c
@@ -248,6 +248,9 @@ int RecursiveLightPoint (mnode_t *node, vec3_t start, vec3_t end)
 	lightplane = plane;
 
 	msurface_t *surf = r_worldmodel->surfaces + node->firstsurface;
+	if (!surf->samples)
+		return 0;
+
 	for (int i = 0; i < node->numsurfaces; i++, surf++)
 	{
 		if (surf->flags & (SURF_DRAWTURB | SURF_DRAWSKY)) 
@@ -255,26 +258,23 @@ int RecursiveLightPoint (mnode_t *node, vec3_t start, vec3_t end)
 
 		mtexinfo_t *tex = surf->texinfo;
 		
-		const int s = DotProduct(mid, tex->vecs[0]) + tex->vecs[0][3];
-		const int t = DotProduct(mid, tex->vecs[1]) + tex->vecs[1][3];;
+		int ds = DotProduct(mid, tex->vecs[0]) + tex->vecs[0][3];
+		int dt = DotProduct(mid, tex->vecs[1]) + tex->vecs[1][3];
 
-		if (s < surf->texturemins[0] ||	t < surf->texturemins[1])
+		if (ds < surf->texturemins[0] || dt < surf->texturemins[1])
 			continue;
 		
-		int ds = s - surf->texturemins[0];
-		int dt = t - surf->texturemins[1];
+		ds -= surf->texturemins[0];
+		dt -= surf->texturemins[1];
 		
 		if (ds > surf->extents[0] || dt > surf->extents[1])
 			continue;
 
-		if (!surf->samples)
-			return 0;
-
-		ds >>= 4;
-		dt >>= 4;
+		ds >>= surf->lmshift; //mxd. Was 4
+		dt >>= surf->lmshift; //mxd. Was 4
 
 		byte *lightmap = surf->samples;
-		lightmap += 3 * (dt * ((surf->extents[0] >> 4) + 1) + ds);
+		lightmap += 3 * (dt * ((surf->extents[0] >> surf->lmshift) + 1) + ds); //mxd. 4 -> lmshift
 
 		VectorCopy(vec3_origin, pointcolor);
 		if (r_newrefdef.lightstyles)
@@ -288,7 +288,7 @@ int RecursiveLightPoint (mnode_t *node, vec3_t start, vec3_t end)
 				for (int c = 0; c < 3; c++) //mxd
 					pointcolor[c] += lightmap[c] * scale[c] * DIV255;
 
-				lightmap += 3 * ((surf->extents[0] >> 4) + 1) * ((surf->extents[1] >> 4) + 1);
+				lightmap += 3 * ((surf->extents[0] >> surf->lmshift) + 1) * ((surf->extents[1] >> surf->lmshift) + 1); //mxd. 4 -> lmshift
 			}
 		}
 		
@@ -613,13 +613,12 @@ R_AddDynamicLights
 void R_AddDynamicLights (msurface_t *surf)
 {
 	vec3_t		impact, local, dlorigin, entOrigin, entAngles;
-	int			s, t;
-	float		fsacc, ftacc;
-	qboolean rotated = false;
-	vec3_t	forward, right, up;
+	qboolean	rotated = false;
+	vec3_t		forward, right, up;
+	const int	lmscale = 1 << surf->lmshift; //mxd
 
-	const int smax = (surf->extents[0] >> 4) + 1;
-	const int tmax = (surf->extents[1] >> 4) + 1;
+	const int smax = (surf->extents[0] >> surf->lmshift) + 1; //mxd. 4 -> lmshift
+	const int tmax = (surf->extents[1] >> surf->lmshift) + 1; //mxd. 4 -> lmshift
 	mtexinfo_t *tex = surf->texinfo;
 
 	// currententity is not valid for trans surfaces
@@ -650,7 +649,7 @@ void R_AddDynamicLights (msurface_t *surf)
 
 	for (int lnum = 0; lnum < r_newrefdef.num_dlights; lnum++)
 	{
-		if ( !(surf->dlightbits[lnum >> 5] & (1 << (lnum & 31)) ) )
+		if ( !(surf->dlightbits[lnum >> 5] & (1U << (lnum & 31)) ) )
 			continue; // not lit by this light
 
 		dlight_t *dl = &r_newrefdef.dlights[lnum];
@@ -689,15 +688,15 @@ void R_AddDynamicLights (msurface_t *surf)
 		local[1] = DotProduct(impact, tex->vecs[1]) + tex->vecs[1][3] - surf->texturemins[1];
 
 		float *pfBL = s_blocklights;
-		for (t = 0, ftacc = 0; t < tmax; t++, ftacc += 16)
+		for (int t = 0; t < tmax; t++)
 		{
-			int td = local[1] - ftacc;
+			int td = local[1] - t * lmscale;
 			if (td < 0)
 				td = -td;
 
-			for (s = 0, fsacc = 0; s < smax; s++, fsacc += 16, pfBL += 3)
+			for (int s = 0; s < smax; s++, pfBL += 3)
 			{
-				int sd = Q_ftol(local[0] - fsacc);
+				int sd = local[0] - s * lmscale; //mxd. Was Q_ftol(local[0] - fsacc); Why Q_ftol?
 
 				if (sd < 0)
 					sd = -sd;
@@ -752,8 +751,8 @@ void R_BuildLightMap (msurface_t *surf, byte *dest, int stride)
 	if (surf->texinfo->flags & (SURF_SKY | SURF_WARP))
 		VID_Error(ERR_DROP, "R_BuildLightMap called for non-lit surface");
 
-	const int smax = (surf->extents[0] >> 4) + 1;
-	const int tmax = (surf->extents[1] >> 4) + 1;
+	const int smax = (surf->extents[0] >> surf->lmshift) + 1; //mxd. 4 -> lmshift
+	const int tmax = (surf->extents[1] >> surf->lmshift) + 1; //mxd. 4 -> lmshift
 	const int size = smax * tmax;
 
 	// FIXME- can this limit be directly increased?		Yep - Knightmare

renderer/r_model.c
@@ -730,23 +730,33 @@ void CalcSurfaceExtents (msurface_t *s)
 		
 		for (int j = 0; j < 2; j++)
 		{
-			const float val = v->position[0] * tex->vecs[j][0] +
-							  v->position[1] * tex->vecs[j][1] +
-							  v->position[2] * tex->vecs[j][2] +
-											   tex->vecs[j][3];
+			/* The following calculation is sensitive to floating-point precision.  It needs to produce the same result that the
+			* light compiler does, because R_BuildLightMap uses surf->extents to know the width/height of a surface's lightmap,
+			* and incorrect rounding here manifests itself as patches of "corrupted" looking lightmaps.
+			* Most light compilers are win32 executables, so they use x87 floating point.  This means the multiplies and adds
+			* are done at 80-bit precision, and the result is rounded down to 32-bits and stored in val.
+			* Adding the casts to double seems to be good enough to fix lighting glitches when Quakespasm is compiled as x86_64
+			* and using SSE2 floating-point.  A potential trouble spot is the hallway at the beginning of mfxsp17.  -- ericw
+			*/
+			const float val = (double)v->position[0] * (double)tex->vecs[j][0] +
+							  (double)v->position[1] * (double)tex->vecs[j][1] +
+							  (double)v->position[2] * (double)tex->vecs[j][2] +
+													   (double)tex->vecs[j][3];
 
 			mins[j] = min(val, mins[j]); //mxd
 			maxs[j] = max(val, maxs[j]); //mxd
 		}
 	}
 
+	const int lmscale = 1 << s->lmshift; //mxd
+
 	for (int i = 0; i < 2; i++)
 	{	
-		const int bmins = floorf(mins[i] / 16);
-		const int bmaxs =  ceilf(maxs[i] / 16);
+		const int bmins = floorf(mins[i] / lmscale); //mxd. 16 -> lmscale
+		const int bmaxs =  ceilf(maxs[i] / lmscale);
 
-		s->texturemins[i] = bmins * 16;
-		s->extents[i] = (bmaxs - bmins) * 16;
+		s->texturemins[i] = bmins * lmscale;
+		s->extents[i] = (bmaxs - bmins) * lmscale;
 	}
 }
 
@@ -760,7 +770,7 @@ void R_BeginBuildingLightmaps (model_t *m);
 Mod_LoadFaces
 =================
 */
-void Mod_LoadFaces (lump_t *l)
+void Mod_LoadFaces (lump_t *l, byte lmshift)
 {
 	dface_t *in = (void *)(mod_base + l->fileofs);
 	if (l->filelen % sizeof(*in))
@@ -782,6 +792,7 @@ void Mod_LoadFaces (lump_t *l)
 		out->numedges = LittleShort(in->numedges);
 		out->flags = 0;
 		out->polys = NULL;
+		out->lmshift = lmshift; //mxd
 
 		const int planenum = (unsigned short)LittleShort(in->planenum);
 		const int side = LittleShort(in->side);
@@ -792,7 +803,7 @@ void Mod_LoadFaces (lump_t *l)
 
 		const int ti = LittleShort(in->texinfo);
 		if (ti < 0 || ti >= loadmodel->numtexinfo)
-			VID_Error(ERR_DROP, "MOD_LoadBmodel: bad texinfo number");
+			VID_Error(ERR_DROP, "Mod_LoadFaces: bad texinfo number");
 
 		out->texinfo = loadmodel->texinfo + ti;
 
@@ -1037,6 +1048,61 @@ void Mod_LoadPlanes (lump_t *l)
 	}
 }
 
+
+/*
+=================
+Mod_ParseWorldspawnKey (https://github.com/Shpoike/Quakespasm/blob/843ba06637951fee6b81958b8c36c718900b752c/quakespasm/Quake/gl_model.c#L1017)
+This just quickly scans the worldspawn entity for a single key. Returning both _prefixed and non prefixed keys.
+(wantkey argument should not have a _prefix.)
+Also, blame Spike!
+=================
+*/
+const char *Mod_ParseWorldspawnKey(lump_t *l, const char *wantkey, char *buffer, size_t sizeofbuffer)
+{
+	// Get entstring...
+	char map_entitystring[MAX_MAP_ENTSTRING];
+	if (l->filelen + 1 > sizeof(map_entitystring))
+		return NULL;
+
+	memcpy(map_entitystring, mod_base + l->fileofs, l->filelen);
+	map_entitystring[l->filelen] = '\0';
+
+	// Try to find wantkey among the first entity props...
+	char foundkey[128];
+	char *data = map_entitystring;
+	char *com_token = COM_Parse(&data);
+
+	if (data && com_token[0] == '{')
+	{
+		while (true)
+		{
+			com_token = COM_Parse(&data);
+
+			if (!data || com_token[0] == '}') // error or end of worldspawn
+				break;
+
+			if (com_token[0] == '_')
+				strcpy(foundkey, com_token + 1);
+			else
+				strcpy(foundkey, com_token);
+
+			com_token = COM_Parse(&data);
+
+			if (!data) // error
+				break; 
+
+			if (!strcmp(wantkey, foundkey))
+			{
+				Q_strncpyz(buffer, com_token, sizeofbuffer);
+				return buffer;
+			}
+		}
+	}
+
+	return NULL;
+}
+
+
 /*
 =================
 Mod_LoadBrushModel
@@ -1060,6 +1126,18 @@ void Mod_LoadBrushModel (model_t *mod, void *buffer)
 	for (int i = 0; i < sizeof(dheader_t) / 4; i++)
 		((int *)header)[i] = LittleLong(((int *)header)[i]);
 
+	//mxd. Get _lightmap_scale form worldspawn...
+	char scalebuf[16];
+	byte lmshift = 4;
+	if (Mod_ParseWorldspawnKey(&header->lumps[LUMP_ENTITIES], "lightmap_scale", scalebuf, sizeof(scalebuf)))
+	{
+		const int value = atoi(scalebuf);
+		for (lmshift = 0; (1 << lmshift) < value && lmshift < 254; lmshift++) { }
+
+		if (value != 16)
+			VID_Printf(PRINT_DEVELOPER, "Using custom lightmap scale (%i) and shift (%i)\n", value, lmshift);
+	}
+
 	// load into heap
 	Mod_LoadVertexes(&header->lumps[LUMP_VERTEXES]);
 	Mod_LoadEdges(&header->lumps[LUMP_EDGES]);
@@ -1067,7 +1145,7 @@ void Mod_LoadBrushModel (model_t *mod, void *buffer)
 	Mod_LoadLighting(&header->lumps[LUMP_LIGHTING]);
 	Mod_LoadPlanes(&header->lumps[LUMP_PLANES]);
 	Mod_LoadTexinfo(&header->lumps[LUMP_TEXINFO]);
-	Mod_LoadFaces(&header->lumps[LUMP_FACES]);
+	Mod_LoadFaces(&header->lumps[LUMP_FACES], lmshift); //mxd. +lmshift
 	Mod_LoadMarksurfaces(&header->lumps[LUMP_LEAFFACES]);
 	Mod_LoadVisibility(&header->lumps[LUMP_VISIBILITY]);
 	Mod_LoadLeafs(&header->lumps[LUMP_LEAFS]);

renderer/r_surface.c
@@ -593,8 +593,8 @@ void R_BlendLightmaps (void)
 
 		for (msurface_t *surf = gl_lms.lightmap_surfaces[0]; surf != 0; surf = surf->lightmapchain)
 		{
-			const int smax = (surf->extents[0] >> 4) + 1;
-			const int tmax = (surf->extents[1] >> 4) + 1;
+			const int smax = (surf->extents[0] >> surf->lmshift) + 1; //mxd. 4 -> lmshift
+			const int tmax = (surf->extents[1] >> surf->lmshift) + 1; //mxd. 4 -> lmshift
 
 			if ( LM_AllocBlock(smax, tmax, &surf->dlight_s, &surf->dlight_t) )
 			{
@@ -682,10 +682,12 @@ void R_RenderBrushPoly (msurface_t *fa)
 	{
 		if ( (fa->styles[maps] >= 32 || fa->styles[maps] == 0) && fa->dlightframe != r_framecount )
 		{
-			unsigned temp[34 * 34];
+			//unsigned temp[34 * 34];
 			
-			const int smax = (fa->extents[0] >> 4) + 1;
-			const int tmax = (fa->extents[1] >> 4) + 1;
+			const int smax = (fa->extents[0] >> fa->lmshift) + 1; //mxd. 4 -> lmshift
+			const int tmax = (fa->extents[1] >> fa->lmshift) + 1; //mxd. 4 -> lmshift
+
+			unsigned *temp = malloc(smax * tmax); //mxd. Was 34 *34
 
 			R_BuildLightMap(fa, (void *)temp, smax * 4);
 			R_SetCacheState(fa);
@@ -2273,6 +2275,7 @@ void R_BuildPolygonFromSurface (msurface_t *fa)
 	// reconstruct the polygon
 	medge_t *pedges = currentmodel->edges;
 	const int lnumverts = fa->numedges;
+	const int lmscale = (1 << fa->lmshift); //mxd
 
 	//
 	// draw texture
@@ -2333,15 +2336,15 @@ void R_BuildPolygonFromSurface (msurface_t *fa)
 		//
 		s = DotProduct(vec, fa->texinfo->vecs[0]) + fa->texinfo->vecs[0][3];
 		s -= fa->texturemins[0];
-		s += fa->light_s * 16;
-		s += 8;
-		s /= LM_BLOCK_WIDTH * 16; //fa->texinfo->texture->width;
+		s += fa->light_s * lmscale; //mxd. 16 -> lmscale
+		s += lmscale * 0.5f; //mxd. Was 8
+		s /= LM_BLOCK_WIDTH * lmscale; //mxd. 16 -> lmscale //fa->texinfo->texture->width;
 
 		t = DotProduct(vec, fa->texinfo->vecs[1]) + fa->texinfo->vecs[1][3];
 		t -= fa->texturemins[1];
-		t += fa->light_t * 16;
-		t += 8;
-		t /= LM_BLOCK_HEIGHT * 16; //fa->texinfo->texture->height;
+		t += fa->light_t * lmscale; //mxd. 16 -> lmscale
+		t += lmscale * 0.5f; //mxd. Was 8
+		t /= LM_BLOCK_HEIGHT * lmscale; //mxd. 16 -> lmscale //fa->texinfo->texture->height;
 
 		poly->verts[i][5] = s;
 		poly->verts[i][6] = t;
@@ -2366,8 +2369,8 @@ void R_CreateSurfaceLightmap (msurface_t *surf)
 	if (surf->texinfo->flags & (SURF_SKY | SURF_WARP))
 		return;
 
-	const int smax = (surf->extents[0] >> 4) + 1;
-	const int tmax = (surf->extents[1] >> 4) + 1;
+	const int smax = (surf->extents[0] >> surf->lmshift) + 1; //mxd. 4 -> lmshift
+	const int tmax = (surf->extents[1] >> surf->lmshift) + 1; //mxd. 4 -> lmshift
 
 	if (!LM_AllocBlock(smax, tmax, &surf->light_s, &surf->light_t))
 	{

renderer/r_local.h
@@ -382,15 +382,15 @@ void R_DrawAllDecals(void);
 //
 // r_light.c
 //
-#define DYNAMIC_LIGHT_WIDTH  128
-#define DYNAMIC_LIGHT_HEIGHT 128
+//#define DYNAMIC_LIGHT_WIDTH  128
+//#define DYNAMIC_LIGHT_HEIGHT 128
 
 #define LIGHTMAP_BYTES 4
 
-#define	LM_BLOCK_WIDTH	128
-#define	LM_BLOCK_HEIGHT	128
+#define	LM_BLOCK_WIDTH	256 //mxd. Was 128
+#define	LM_BLOCK_HEIGHT	256 //mxd. Was 128
 
-//#define	MAX_LIGHTMAPS	128	// moved this up above to keeep TEXNUM_* values aligned
+//#define	MAX_LIGHTMAPS	128	// moved this up above to keep TEXNUM_* values aligned
 
 #define GL_LIGHTMAP_FORMAT	GL_BGRA // was GL_RGBA
 #define GL_LIGHTMAP_TYPE	GL_UNSIGNED_INT_8_8_8_8_REV	// was GL_UNSIGNED_BYTE
@@ -416,9 +416,8 @@ typedef struct
 
 	int			allocated[LM_BLOCK_WIDTH];
 
-	// the lightmap texture data needs to be kept in
-	// main memory so texsubimage can update properly
-	unsigned	lightmap_buffer[LM_BLOCK_WIDTH*LM_BLOCK_HEIGHT];
+	// the lightmap texture data needs to be kept in main memory, so texsubimage can update properly
+	unsigned	lightmap_buffer[LM_BLOCK_WIDTH * LM_BLOCK_HEIGHT];
 #ifdef BATCH_LM_UPDATES	// Knightmare added
 	unsigned	*lightmap_update[MAX_LIGHTMAPS];
 	rect_t		lightrect[MAX_LIGHTMAPS];

renderer/r_model.h
@@ -136,6 +136,8 @@ typedef struct msurface_s
 	int			light_smax, light_tmax;
 	int			dlight_s, dlight_t; // gl lightmap coordinates for dynamic lightmaps
 
+	byte		lmshift; //mxd
+
 	glpoly_t	*polys;				// multiple if warped
 	struct	msurface_s	*texturechain;
 	struct  msurface_s	*lightmapchain;

commit 3614ac57601cc6bf4a4d9335af72fb8900610477

Fixed incorrect vertex colors on warped/scrolling surfaces introduced in 399aab2.

renderer/r_light.c
@@ -247,11 +247,12 @@ int RecursiveLightPoint (vec3_t color, mnode_t *node, vec3_t start, vec3_t end)
 	lightplane = plane;
 
 	msurface_t *surf = r_worldmodel->surfaces + node->firstsurface;
-	if (!surf->samples)
-		return 0;
 
 	for (int i = 0; i < node->numsurfaces; i++, surf++)
 	{
+		if (!surf->samples)
+			continue; //mxd. No lightmap data. Was return 0;
+		
 		if (surf->flags & (SURF_DRAWTURB | SURF_DRAWSKY)) 
 			continue;	// no lightmaps
 
@@ -356,11 +357,15 @@ void R_LightPoint (vec3_t p, vec3_t color, qboolean isEnt)
 	vec3_t end = { p[0], p[1], p[2] - 8192 }; //mxd. Was p[2] - 2048
 	const float r = RecursiveLightPoint(color, r_worldmodel->nodes, p, end);
 	if (r == -1)
+	{
 		VectorClear(color);
-
-	// this catches too bright modulated color
-	for (int i = 0; i < 3; i++)
-		color[i] = min(color[i], 1);
+	}
+	else
+	{
+		// this catches too bright modulated color
+		for (int i = 0; i < 3; i++)
+			color[i] = min(color[i], 1);
+	}
 
 	// add dynamic lights
 	dlight_t *dl = r_newrefdef.dlights;
@@ -398,11 +403,15 @@ void R_LightPointDynamics (vec3_t p, vec3_t color, m_dlight_t *list, int *amount
 	vec3_t end = { p[0], p[1], p[2] - 8192 }; //mxd. Was p[2] - 2048
 	const float r = RecursiveLightPoint(color, r_worldmodel->nodes, p, end);
 	if (r == -1)
+	{
 		VectorClear(color);
-
-	// this catches too bright modulated color
-	for (int i = 0; i < 3; i++)
-		color[i] = min(color[i], 1);
+	}
+	else
+	{
+		// this catches too bright modulated color
+		for (int i = 0; i < 3; i++)
+			color[i] = min(color[i], 1);
+	}
 
 	// add dynamic lights
 	int m_dl = 0;
@@ -476,11 +485,15 @@ void R_SurfLightPoint (msurface_t *surf, vec3_t p, vec3_t color, qboolean baseli
 		vec3_t end = { p[0], p[1], p[2] - 8192 }; //mxd. Was p[2] - 2048
 		const float r = RecursiveLightPoint(color, r_worldmodel->nodes, p, end);
 		if (r == -1)
+		{
 			VectorClear(color);
-
-		// this catches too bright modulated color
-		for (int i = 0; i < 3; i++)
-			color[i] = min(color[i], 1);
+		}
+		else
+		{
+			// this catches too bright modulated color
+			for (int i = 0; i < 3; i++)
+				color[i] = min(color[i], 1);
+		}
 	}
 	else
 	{
@@ -594,7 +607,7 @@ void R_ShadowLight (vec3_t pos, vec3_t lightAdd)
 
 //===================================================================
 
-static float s_blocklights[128 * 128 * 4]; //Knightmare-  was [34*34*3], supports max chop size of 2048?
+static float s_blocklights[LM_BLOCK_WIDTH * LM_BLOCK_HEIGHT * 4]; //mxd. Was [128*128*4] //Knightmare-  was [34*34*3], supports max chop size of 2048?
 /*
 ===============
 R_AddDynamicLights

renderer/r_model.c
@@ -279,7 +279,7 @@ model_t *Mod_ForName (char *name, qboolean crash)
 		break;
 	
 	case IDBSPHEADER:
-		loadmodel->extradata = Hunk_Begin(0x2000000); //mxd. Was 0x1000000
+		loadmodel->extradata = Hunk_Begin(0x4000000); //mxd. Was 0x1000000
 		Mod_LoadBrushModel(mod, buf);
 		break;
 

renderer/r_surface.c
@@ -2028,38 +2028,21 @@ R_BuildVertexLightBase
 */
 void R_SurfLightPoint (msurface_t *surf, vec3_t p, vec3_t color, qboolean baselight);
 
-qboolean R_BuildVertexLightBase (msurface_t *surf, glpoly_t *poly)
+void R_BuildVertexLightBase (msurface_t *surf, glpoly_t *poly)
 {
-	int			i;
-	float		*v;
-	qboolean	lit = false;
-
-	for (i = 0, v = poly->verts[0]; i < poly->numverts; i++, v += VERTEXSIZE)
+	float *v = poly->verts[0];
+	for (int i = 0; i < poly->numverts; i++, v += VERTEXSIZE)
 	{
 		vec3_t color, point;
 		VectorCopy(v, point); // lerp outward away from plane to avoid dark spots?
-		// lerp between each vertex and origin - use check for too dark?
-		// this messes up curved glass surfaces
-		//VectorSubtract (poly->center, v, point);
-		//VectorMA(v, 0.01, point, point);
+		VectorClear(color); //mxd
 
 		R_SurfLightPoint(surf, point, color, true);
 		R_MaxColorVec(color);
 
-		for (int c = 0; c < 3; c++)
-		{
-			if (color[c] > 0.0f)
-			{
-				lit = true;
-				break;
-			}
-		}
-
 		for (int c = 0; c < 3; c++) //mxd
 			poly->vertexlightbase[i * 3 + c] = (byte)(color[c] * 255.0);
 	}
-
-	return lit;
 }
 
 
@@ -2085,10 +2068,6 @@ R_BuildVertexLight
 */
 void R_BuildVertexLight (msurface_t *surf)
 {
-	vec3_t		color, point;
-	int			i;
-	float		*v;
-
 	if (surf->flags & SURF_DRAWTURB)
 	{
 		if (!r_warp_lighting->value) return;
@@ -2110,24 +2089,19 @@ void R_BuildVertexLight (msurface_t *surf)
 		{	
 			R_BuildVertexLightBase(surf, poly);
 			poly->vertexlightset = true;
-		//	if (R_BuildVertexLightBase(surf, poly))
-		//		poly->vertexlightset = true;
-		//	else
-		//		return; // don't bother if lightbase is all black
 		}
 
-		for (i = 0, v = poly->verts[0]; i < poly->numverts; i++, v += VERTEXSIZE)
+		float *v = poly->verts[0];
+		for (int i = 0; i < poly->numverts; i++, v += VERTEXSIZE)
 		{
+			vec3_t color, point;
 			VectorCopy(v, point); // lerp outward away from plane to avoid dark spots?
-			// lerp between each vertex and origin - use check for too dark?
-			// this messes up curved glass surfaces
-			//VectorSubtract (poly->center, v, point);
-			//VectorMA(v, 0.01, point, point);
+			VectorClear(color); //mxd
 
 			R_SurfLightPoint(surf, point, color, false);
 
 			for (int c = 0; c < 3; c++) //mxd
-				color[c] = (float)poly->vertexlightbase[i * 3 + c] / 255.0 + color[c];
+				color[c] += (float)poly->vertexlightbase[i * 3 + c] / 255.0;
 				
 			R_MaxColorVec(color);
 