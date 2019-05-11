m-x-d/KMQuake2

commit 7cfca78d405f0bcb49fd7fe0b95086b5e67e7649

FS_AddGameDirectory(): directory is now added last to the search list (so it's searched first). Fixes incorrect maps.lst loaded when using patched version of Q2, allows to override KMQ2 assets by placing files in baseq2 directory. FS_AddGameDirectory(): bug? Last found filename was skipped when adding extra .pak/.pk3 files. files.c: file_from_pk3 var wasn't used. Replaced with file_from_pak var. files.c: removed FS_FTell() (was identical to FS_Tell(), was used only in cl_cimenatic.c).

qcommon/files.c
@@ -104,10 +104,10 @@ typedef struct fsSearchPath_s
 } fsSearchPath_t;
 
 
-fsHandle_t		fs_handles[MAX_HANDLES];
-fsLink_t		*fs_links;
-fsSearchPath_t	*fs_searchPaths;
-fsSearchPath_t	*fs_baseSearchPaths;
+static fsHandle_t		fs_handles[MAX_HANDLES];
+static fsLink_t			*fs_links;
+static fsSearchPath_t	*fs_searchPaths;
+static fsSearchPath_t	*fs_baseSearchPaths;
 
 char			fs_gamedir[MAX_OSPATH];
 static char		fs_currentGame[MAX_QPATH];
@@ -116,8 +116,7 @@ static char		fs_fileInPath[MAX_OSPATH];
 static qboolean	fs_fileInPack;
 
 // Set by FS_FOpenFile
-int file_from_pak = 0;
-int file_from_pk3 = 0;
+qboolean file_from_pak;
 char last_pk3_name[MAX_QPATH];
 
 cvar_t	*fs_homepath;
@@ -131,7 +130,7 @@ cvar_t	*fs_roguegame;
 
 
 // Returns the path up to, but not including the last /
-void Com_FilePath(const char *path, char *dst, int dstSize)
+static void Com_FilePath(const char *path, char *dst, int dstSize)
 {
 	const char *last;
 
@@ -148,7 +147,7 @@ void Com_FilePath(const char *path, char *dst, int dstSize)
 }
 
 // Returns bit flag based on pak item's extension.
-unsigned int FS_TypeFlagForPakItem(char *itemName)
+static unsigned int FS_TypeFlagForPakItem(char *itemName)
 {
 	static const char *type_extensions[] =
 	{
@@ -166,7 +165,7 @@ unsigned int FS_TypeFlagForPakItem(char *itemName)
 	return 0;
 }
 
-int FS_FileLength(FILE *f)
+static int FS_FileLength(FILE *f)
 {
 	const int cur = ftell(f);
 	fseek(f, 0, SEEK_END);
@@ -244,7 +243,7 @@ void FS_DeletePath(char *path)
 }
 
 // Returns a fsHandle_t * for the given fileHandle_t
-fsHandle_t *FS_GetFileByHandle(fileHandle_t f)
+static fsHandle_t *FS_GetFileByHandle(fileHandle_t f)
 {
 	if (f <= 0 || f > MAX_HANDLES)
 		Com_Error(ERR_DROP, "FS_GetFileByHandle: out of range");
@@ -267,7 +266,7 @@ FILE *FS_FileForHandle(fileHandle_t f)
 }
 
 // Finds a free fileHandle_t
-fsHandle_t *FS_HandleForFile(const char *path, fileHandle_t *f)
+static fsHandle_t *FS_HandleForFile(const char *path, fileHandle_t *f)
 {
 	fsHandle_t *handle = fs_handles;
 	for (int i = 0; i < MAX_HANDLES; i++, handle++)
@@ -289,7 +288,7 @@ fsHandle_t *FS_HandleForFile(const char *path, fileHandle_t *f)
 #ifdef BINARY_PACK_SEARCH
 
 // Performs a binary search by hashed filename to find pack items in a sorted pack
-int FS_FindPackItem(fsPack_t *pack, char *itemName, long itemHash)
+static int FS_FindPackItem(fsPack_t *pack, char *itemName, long itemHash)
 {
 	// Catch null pointers
 	if (!pack || !itemName)
@@ -323,7 +322,7 @@ int FS_FindPackItem(fsPack_t *pack, char *itemName, long itemHash)
 #endif	// BINARY_PACK_SEARCH
 
 // Returns file size or -1 on error
-int FS_FOpenFileAppend(fsHandle_t *handle)
+static int FS_FOpenFileAppend(fsHandle_t *handle)
 {
 	FS_CreatePath(handle->name);
 
@@ -346,7 +345,7 @@ int FS_FOpenFileAppend(fsHandle_t *handle)
 }
 
 // Returns 0 on success, -1 on error
-int FS_FOpenFileWrite(fsHandle_t *handle)
+static int FS_FOpenFileWrite(fsHandle_t *handle)
 {
 	FS_CreatePath(handle->name);
 
@@ -370,11 +369,10 @@ int FS_FOpenFileWrite(fsHandle_t *handle)
 
 // Returns file size or -1 if not found.
 // Can open separate files as well as files inside pack files (both PAK and PK3).
-int FS_FOpenFileRead(fsHandle_t *handle)
+static int FS_FOpenFileRead(fsHandle_t *handle)
 {
 	// Knightmare- hack global vars for autodownloads
-	file_from_pak = 0;
-	file_from_pk3 = 0;
+	file_from_pak = false;
 
 	Com_sprintf(last_pk3_name, sizeof(last_pk3_name), "\0");
 	const long hash = Com_HashFileName(handle->name, 0, false);
@@ -432,7 +430,7 @@ int FS_FOpenFileRead(fsHandle_t *handle)
 					else if (pack->pk3)
 					{
 						// PK3
-						file_from_pk3 = true; // Knightmare added
+						file_from_pak = true; // Knightmare added //BUG: mxd. This was file_from_pk3 in KMQ2 and it was never used
 						Com_sprintf(last_pk3_name, sizeof(last_pk3_name), strrchr(pack->name, '/') + 1); // Knightmare added
 						handle->zip = unzOpen(pack->name);
 						if (handle->zip)
@@ -602,19 +600,6 @@ int FS_Write(const void *buffer, int size, fileHandle_t f)
 	}
 
 	return size;
-}
-
-int FS_FTell(fileHandle_t f)
-{
-	fsHandle_t *handle = FS_GetFileByHandle(f);
-
-	if (handle->file)
-		return ftell(handle->file);
-
-	if (handle->zip)
-		return unztell(handle->zip);
-
-	return 0;
 }
 
 // Generates a listing of the contents of a pak file
@@ -826,7 +811,7 @@ void FS_FreeFile(void *buffer)
 }
 
 // Checks against a blacklist to see if a file should not be loaded from a pak.
-qboolean FS_FileInPakBlacklist(char *filename, qboolean isPk3)
+static qboolean FS_FileInPakBlacklist(char *filename, qboolean isPk3)
 {
 	// Some incompetently packaged mods have these files in their paks!
 	static char *pakfile_ignore_names[] =
@@ -861,9 +846,9 @@ qboolean FS_FileInPakBlacklist(char *filename, qboolean isPk3)
 #ifdef BINARY_PACK_SEARCH
 
 // Used for sorting pak entries by hash 
-long *nameHashes = NULL;
+static long *nameHashes = NULL;
 
-int FS_PakFileCompare(const void *f1, const void *f2)
+static int FS_PakFileCompare(const void *f1, const void *f2)
 {
 	if (!nameHashes)
 		return 1;
@@ -875,11 +860,11 @@ int FS_PakFileCompare(const void *f1, const void *f2)
 
 // Takes an explicit (not game tree related) path to a pack file.
 // Loads the header and directory, adding the files at the beginning of the list so they override previous pack files.
-fsPack_t *FS_LoadPAK(const char *packPath)
+static fsPack_t *FS_LoadPAK(const char *packPath)
 {
-	dpackheader_t	header;
-	dpackfile_t		info[MAX_FILES_IN_PACK];
-	unsigned		contentFlags = 0;
+	dpackheader_t header;
+	dpackfile_t info[MAX_FILES_IN_PACK];
+	unsigned contentFlags = 0;
 
 	FILE *handle = fopen(packPath, "rb");
 	if (!handle)
@@ -983,12 +968,12 @@ void FS_AddPAKFile(const char *packPath)
 
 // Takes an explicit (not game tree related) path to a pack file.
 // Loads the header and directory, adding the files at the beginning of the list so they override previous pack files.
-fsPack_t *FS_LoadPK3(const char *packPath)
+static fsPack_t *FS_LoadPK3(const char *packPath)
 {
-	unz_global_info	global;
-	unz_file_info	info;
-	unsigned		contentFlags = 0;
-	char			fileName[MAX_QPATH];
+	unz_global_info global;
+	unz_file_info info;
+	unsigned contentFlags = 0;
+	char fileName[MAX_QPATH];
 
 	unzFile *handle = unzOpen(packPath);
 	if (!handle)
@@ -1004,7 +989,7 @@ fsPack_t *FS_LoadPK3(const char *packPath)
 	if (numFiles > MAX_FILES_IN_PACK || numFiles == 0)
 	{
 		unzClose(handle);
-		Com_Error(ERR_FATAL, "FS_LoadPK3: %s has %i files", packPath, numFiles);
+		Com_Error(ERR_FATAL, "FS_LoadPK3: %s has too many files or no files at all (%i / %i)", packPath, numFiles, MAX_FILES_IN_PACK);
 	}
 
 	fsPackFile_t *files = Z_Malloc(numFiles * sizeof(fsPackFile_t));
@@ -1106,49 +1091,36 @@ void FS_AddPK3File(const char *packPath)
 }
 
 // Sets fs_gameDir, adds the directory to the head of the path, then loads and adds all the pack files found (in alphabetical order).
-// PK3 files are loaded later so they override PAK files.
-void FS_AddGameDirectory(const char *dir)
+static void FS_AddGameDirectory(const char *dir)
 {
-	char packPath[MAX_OSPATH];
-
-	// VoiD -S- *.pak support
-	char findname[1024];
-	int ndirs;
-	// VoiD -E- *.pak support
+	char packpath[MAX_OSPATH];
 
 	Q_strncpyz(fs_gamedir, dir, sizeof(fs_gamedir));
 
-	// Add the directory to the search path
-	fsSearchPath_t *search = Z_Malloc(sizeof(fsSearchPath_t));
-	Q_strncpyz(search->path, dir, sizeof(search->path));
-	search->path[sizeof(search->path) - 1] = 0;
-	search->next = fs_searchPaths;
-	fs_searchPaths = search;
-
-	// Add any pak files in the format pak0.pak pak1.pak, ...
+	// Add pak files in the pak0.pak, pak1.pak... format
 	for (int i = 0; i < 100; i++) // Pooy - paks can now go up to 100
 	{
-		Com_sprintf(packPath, sizeof(packPath), "%s/pak%i.pak", dir, i);
-		FS_AddPAKFile(packPath);
+		Com_sprintf(packpath, sizeof(packpath), "%s/pak%i.pak", dir, i);
+		FS_AddPAKFile(packpath);
 	}
 
-	// NeVo - pak3's!
-	// Add any pk3 files in the format pak0.pk3 pak1.pk3, ...
+	//NeVo. Add pk3 files in the pak0.pk3, pak1.pk3... format 
 	for (int i = 0; i < 100; i++) // Pooy - paks can now go up to 100
 	{
-		Com_sprintf(packPath, sizeof(packPath), "%s/pak%i.pk3", dir, i);
-		FS_AddPK3File(packPath);
+		Com_sprintf(packpath, sizeof(packpath), "%s/pak%i.pk3", dir, i);
+		FS_AddPK3File(packpath);
 	}
 
-	for (int i = 0; i < 2; i++)
+	// Add pak files not using pak0.pak, pak1.pak... format, then add pk3 files not using pak0.pk3, pak1.pk3... format
+	for (int ispak = 0; ispak < 2; ispak++)
 	{
-		// NeVo - Set filetype
-		if(i == 0)
-			Com_sprintf(findname, sizeof(findname), "%s/%s", dir, "*.pak"); // Standard Quake II pack file '.pak'
-		else
-			Com_sprintf(findname, sizeof(findname), "%s/%s", dir, "*.pk3"); // Quake III pack file '.pk3'
+		const char *ext = (ispak ?  "pak" : "pk3");
 
-		// VoiD -S- *.pack support
+		// Set search mask
+		char findname[1024];
+		Com_sprintf(findname, sizeof(findname), "%s/*.%s", dir, ext);
+
+		// Use correct slashes...
 		char *tmp = findname;
 		while (*tmp != 0)
 		{
@@ -1158,45 +1130,52 @@ void FS_AddGameDirectory(const char *dir)
 			tmp++;
 		}
 
-		char **dirnames = FS_ListFiles(findname, &ndirs, 0, 0);
-		if (dirnames)
+		// Find all .pak/.pk3 files
+		int numfiles;
+		char **filenames = FS_ListFiles(findname, &numfiles, 0, 0);
+		if (filenames == NULL)
+			return;
+
+		// Add the ones, which don't match pakX naming sceme
+		for (int i = 0; i < numfiles; i++) //BUG? mxd. This counted to numfiles - 1 in KMQ2. Why?
 		{
-			for (int j = 0; j < ndirs - 1; j++)
+			qboolean numberedpak = false;
+
+			// Skip packs using pakX naming scheme...
+			for (int j = 0; j < 100; j++)
 			{
 				char buf[16];
-				char buf2[16];
-				qboolean numberedpak = false;
+				Com_sprintf(buf, sizeof(buf), "/pak%i.%s", j, ext);
 
-				for (int k = 0; k < 100; k++)
+				if (strstr(filenames[i], buf))
 				{
-					Com_sprintf(buf, sizeof(buf), "/pak%i.pak", k);
-					Com_sprintf(buf2, sizeof(buf2), "/pak%i.pk3", k);
-
-					if (strstr(dirnames[j], buf) || strstr(dirnames[j], buf2))
-					{
-						numberedpak = true;
-						break;
-					}
+					numberedpak = true;
+					break;
 				}
-
-				if (numberedpak)
-					continue;
-
-				if (strrchr(dirnames[j], '/'))
-				{
-					if (i == 1)
-						FS_AddPK3File(dirnames[j]);
-					else
-						FS_AddPAKFile(dirnames[j]);
-				}
-
-				free(dirnames[j]);
 			}
 
-			free(dirnames);
+			if (numberedpak)
+				continue;
+
+			if (strrchr(filenames[i], '/'))
+			{
+				if (ispak)
+					FS_AddPAKFile(filenames[i]);
+				else
+					FS_AddPK3File(filenames[i]);
+			}
+
+			free(filenames[i]);
 		}
-		// VoiD -E- *.pack support
+
+		free(filenames);
 	}
+
+	// Add the directory to the search path //mxd. Add last, so it's checked first
+	fsSearchPath_t *search = Z_Malloc(sizeof(fsSearchPath_t));
+	Q_strncpyz(search->path, dir, sizeof(search->path));
+	search->next = fs_searchPaths;
+	fs_searchPaths = search;
 }
 
 // Allows enumerating all of the directories in the search path
@@ -1220,7 +1199,7 @@ char *FS_NextPath(const char *prevPath)
 	return NULL;
 }
 
-void FS_Path_f(void)
+static void FS_Path_f(void)
 {
 	int totalFiles = 0;
 
@@ -1254,7 +1233,7 @@ void FS_Path_f(void)
 }
 
 // Creates a filelink_t
-void FS_Link_f(void)
+static void FS_Link_f(void)
 {
 	if (Cmd_Argc() != 3)
 	{
@@ -1551,47 +1530,44 @@ char **FS_ListFiles(char *findname, int *numfiles, unsigned musthave, unsigned c
 {
 	int nfiles = 0;
 
-	char *s = Sys_FindFirst(findname, musthave, canthave);
-	while (s)
+	// Count number of matching files
+	char *filename = Sys_FindFirst(findname, musthave, canthave);
+	while (filename != NULL)
 	{
-		if (s[strlen(s) - 1] != '.')
+		if (filename[strlen(filename) - 1] != '.')
 			nfiles++;
 
-		s = Sys_FindNext(musthave, canthave);
+		filename = Sys_FindNext(musthave, canthave);
 	}
-
 	Sys_FindClose();
 
-	if (!nfiles)
-	{
-		*numfiles = 0;
-		return NULL;
-	}
-
-	nfiles++; // Add space for a guard
+	// Store number of matching files
 	*numfiles = nfiles;
 
-	char **list = malloc(sizeof( char * ) * nfiles);
-	memset(list, 0, sizeof(char *) * nfiles);
+	if (nfiles == 0)
+		return NULL;
 
-	s = Sys_FindFirst(findname, musthave, canthave);
-	nfiles = 0;
-	while (s)
+	// Fill list with matching filenames
+	const int listsize = sizeof(char *) * (nfiles + 1); // Add space for a guard
+	char **list = malloc(listsize);
+	memset(list, 0, listsize);
+
+	filename = Sys_FindFirst(findname, musthave, canthave);
+	for (int i = 0; i < nfiles && filename != NULL; i++)
 	{
-		if (s[strlen(s) - 1] != '.')
+		if (filename[strlen(filename) - 1] != '.')
 		{
-			list[nfiles] = strdup(s);
+			list[i] = strdup(filename);
 #ifdef _WIN32
-			strlwr(list[nfiles]);
+			strlwr(list[i]);
 #endif
-			nfiles++;
 		}
 
-		s = Sys_FindNext(musthave, canthave);
+		filename = Sys_FindNext(musthave, canthave);
 	}
-
 	Sys_FindClose();
 
+	// Return result
 	return list;
 }
 
@@ -1635,7 +1611,7 @@ void FS_InsertInList(char **list, char *insert, int len, int start)
 	list[len] = strdup(insert);
 }
 
-void FS_Dir_f(void)
+static void FS_Dir_f(void)
 {
 	char *path = NULL;
 	char findname[1024];

client/cl_cinematic.c
@@ -1181,7 +1181,9 @@ cinHandle_t CIN_PlayCinematic(const char *name, int x, int y, int w, int h, int 
 
 		CIN_Huff1TableInit(cin);
 
-		cin->start = FS_FTell(cin->file);
+		cin->start = FS_Tell(cin->file); //mxd. FS_FTell -> FS_Tell
+		if (cin->start == -1) //mxd
+			return 0;
 	}
 	else
 	{
@@ -1191,7 +1193,9 @@ cinHandle_t CIN_PlayCinematic(const char *name, int x, int y, int w, int h, int 
 		CIN_ReadChunk(cin);
 		CIN_SoundSqrTableInit(cin);
 
-		cin->start = FS_FTell(cin->file);
+		cin->start = FS_Tell(cin->file); //mxd. FS_FTell -> FS_Tell
+		if (cin->start == -1) //mxd
+			return 0;
 	}
 
 	cin->frameCount = 0;

qcommon/qcommon.h
@@ -738,8 +738,7 @@ typedef enum
 	FS_SEARCH_FULL_PATH
 } fsSearchType_t;
 
-extern int file_from_pak;
-extern int file_from_pk3;
+extern qboolean file_from_pak;
 extern char last_pk3_name[MAX_QPATH];
 
 void FS_Startup(void);
@@ -753,7 +752,6 @@ void FS_FCloseFile(fileHandle_t f);
 int FS_Read(void *buffer, int size, fileHandle_t f);
 int FS_Write(const void *buffer, int size, fileHandle_t f);
 void FS_Seek(fileHandle_t f, int offset, fsOrigin_t origin);
-int FS_FTell(fileHandle_t f);
 int FS_Tell(fileHandle_t f);
 qboolean FS_FileExists(char *path);
 qboolean FS_LocalFileExists(char *path);

commit fd12032913a0d056ecb94442db21655f04eb380f

FS_ListFiles(): fixed incorrect list initialization introduced in 7cfca78. Fixed some bugs related to FS_ListFiles() numfiles handling change introduced in 7cfca78. ui_mp_playersetup.c, ModelCallback(): update numitemnames to make SpinControl_DoSlide correctly wrap around when navigating backwards. ui_mp_playersetup.c, pmicmpfnc(): sort by male, female, cyborg then alphabetical. ui_mp_playersetup.c: rewritten parts of PlayerConfig_ScanDirectories() for clarity.

qcommon/files.c
@@ -1137,7 +1137,7 @@ static void FS_AddGameDirectory(const char *dir)
 			return;
 
 		// Add the ones, which don't match pakX naming sceme
-		for (int i = 0; i < numfiles; i++) //BUG? mxd. This counted to numfiles - 1 in KMQ2. Why?
+		for (int i = 0; i < numfiles; i++)
 		{
 			qboolean numberedpak = false;
 
@@ -1526,15 +1526,17 @@ void FS_ExecAutoexec(void)
 	Sys_FindClose();
 }
 
+//mxd. Returns number of matching files, not number of matching files + 1, like in Vanilla!
 char **FS_ListFiles(char *findname, int *numfiles, unsigned musthave, unsigned canthave)
 {
 	int nfiles = 0;
-
+	
 	// Count number of matching files
 	char *filename = Sys_FindFirst(findname, musthave, canthave);
 	while (filename != NULL)
 	{
-		if (filename[strlen(filename) - 1] != '.')
+		// Sys_FindFirst / Sys_FindNext can return a relative path to current ("findname\.") or parent ("findname\..") directory. Skip those.
+		if (filename[strlen(filename) - 1] != '.') 
 			nfiles++;
 
 		filename = Sys_FindNext(musthave, canthave);
@@ -1547,13 +1549,14 @@ char **FS_ListFiles(char *findname, int *numfiles, unsigned musthave, unsigned c
 	if (nfiles == 0)
 		return NULL;
 
-	// Fill list with matching filenames
-	const int listsize = sizeof(char *) * (nfiles + 1); // Add space for a guard
+	// Allocate list
+	const size_t listsize = sizeof(char *) * nfiles;
 	char **list = malloc(listsize);
 	memset(list, 0, listsize);
 
+	// Fill list with matching filenames
 	filename = Sys_FindFirst(findname, musthave, canthave);
-	for (int i = 0; i < nfiles && filename != NULL; i++)
+	for(int i = 0; filename != NULL;)
 	{
 		if (filename[strlen(filename) - 1] != '.')
 		{
@@ -1561,6 +1564,7 @@ char **FS_ListFiles(char *findname, int *numfiles, unsigned musthave, unsigned c
 #ifdef _WIN32
 			strlwr(list[i]);
 #endif
+			i++;
 		}
 
 		filename = Sys_FindNext(musthave, canthave);
@@ -1640,10 +1644,10 @@ static void FS_Dir_f(void)
 		char **dirnames = FS_ListFiles(findname, &ndirs, 0, 0);
 		if (dirnames)
 		{
-			for (int i = 0; i < ndirs-1; i++)
+			for (int i = 0; i < ndirs; i++)
 			{
-				if (strrchr(dirnames[i], '/' ))
-					Com_Printf("%s\n", strrchr(dirnames[i], '/') + 1 );
+				if (strrchr(dirnames[i], '/'))
+					Com_Printf("%s\n", strrchr(dirnames[i], '/') + 1);
 				else
 					Com_Printf("%s\n", dirnames[i]);
 

ui/ui_local.h
@@ -117,7 +117,7 @@ typedef struct
 
 	int curvalue;
 
-	const char **itemnames;
+	const char **itemnames; // Last item is expected to be "\0"
 	int numitemnames;
 } menulist_s;
 

ui/ui_mp_playersetup.c
@@ -81,6 +81,7 @@ static void ModelCallback(void *unused)
 	char scratch[MAX_QPATH];
 
 	s_player_skin_box.itemnames = s_pmi[s_player_model_box.curvalue].skindisplaynames;
+	s_player_skin_box.numitemnames = s_pmi[s_player_model_box.curvalue].nskins; //mxd. Added to make SpinControl_DoSlide correctly wrap around when navigating backwards
 	s_player_skin_box.curvalue = 0;
 
 	// Only register model and skin on starup or when changed
@@ -179,32 +180,31 @@ static qboolean IsValidSkin(char **filelist, int numFiles, int index)
 	return false;
 }
 
-static qboolean PlayerConfig_ScanDirectories(void)
+static void PlayerConfig_ScanDirectories(void)
 {
-	char findname[1024];
-	char scratch[1024];
-	int ndirs = 0;
-	char **dirnames;
 	char *path = NULL;
-
+	
 	s_numplayermodels = 0;
 
 	// Loop back to here if there were no valid player models found in the selected path
-	do
+	while(true)
 	{
+		int ndirs;
+		char **dirnames = NULL;
+		
 		// Get a list of directories
-		do 
+		while((path = FS_NextPath(path)) != NULL)
 		{
-			path = FS_NextPath(path);
+			char findname[1024];
 			Com_sprintf(findname, sizeof(findname), "%s/players/*.*", path);
 
 			dirnames = FS_ListFiles(findname, &ndirs, SFF_SUBDIR, 0);
 			if (dirnames)
 				break;
-		} while (path);
+		}
 
-		if (!dirnames)
-			return false;
+		if (dirnames == NULL)
+			return;
 
 		// Go through the subdirectories
 		int npms = min(ndirs, MAX_PLAYERMODELS);
@@ -235,6 +235,7 @@ static qboolean PlayerConfig_ScanDirectories(void)
 				continue; // todo: add any skins for this model not already listed to skindisplaynames
 
 			// Verify the existence of tris.md2
+			char scratch[1024];
 			Q_strncpyz(scratch, dirnames[i], sizeof(scratch));
 			Q_strncatz(scratch, "/tris.md2", sizeof(scratch));
 			if (!Sys_FindFirst(scratch, 0, SFF_SUBDIR | SFF_HIDDEN | SFF_SYSTEM))
@@ -262,36 +263,34 @@ static qboolean PlayerConfig_ScanDirectories(void)
 
 			// Count valid skins, which consist of a skin with a matching "_i" icon
 			int nskins = 0;
-			for (int k = 0; k < nimagefiles - 1; k++) //TODO: mxd. Why nimagefiles - 1, not nimagefiles?
+			for (int k = 0; k < nimagefiles; k++)
 				if (IsValidSkin(imagenames, nimagefiles, k))
 					nskins++;
 
-			if (!nskins)
+			if (nskins == 0)
 				continue;
 
+			// Add extra empty item at the end, because Menu_AddItem() counts number of spincontrol items by counting itemnames to '\0' item
 			char **skinnames = malloc(sizeof(char *) * (nskins + 1));
 			memset(skinnames, 0, sizeof(char *) * (nskins + 1));
 
 			// Copy the valid skins
-			if (nimagefiles > 0)
+			int skinindex = 0;
+			for (int k = 0; k < nimagefiles; k++)
 			{
-				int skinindex = 0;
-				for (int k = 0; k < nimagefiles - 1; k++)
+				if (IsValidSkin(imagenames, nimagefiles, k))
 				{
-					if (IsValidSkin(imagenames, nimagefiles, k))
-					{
-						char *a2 = strrchr(imagenames[k], '/');
-						char *b2 = strrchr(imagenames[k], '\\');
-						char *c2 = max(a2, b2);
+					char *a2 = strrchr(imagenames[k], '/');
+					char *b2 = strrchr(imagenames[k], '\\');
+					char *c2 = max(a2, b2);
 
-						Q_strncpyz(scratch, c2 + 1, sizeof(scratch));
+					Q_strncpyz(scratch, c2 + 1, sizeof(scratch));
 
-						if (strrchr(scratch, '.'))
-							*strrchr(scratch, '.') = 0;
+					if (strrchr(scratch, '.'))
+						*strrchr(scratch, '.') = 0;
 
-						skinnames[skinindex] = strdup(scratch);
-						skinindex++;
-					}
+					skinnames[skinindex] = strdup(scratch);
+					skinindex++;
 				}
 			}
 
@@ -314,11 +313,7 @@ static qboolean PlayerConfig_ScanDirectories(void)
 		
 		if (dirnames)
 			FS_FreeFileList(dirnames, ndirs);
-
-	// If no valid player models found in path, try next path, if there is one
-	} while (path);	// (s_numplayermodels == 0 && path);
-
-	return true; //** DMP warning fix
+	}
 }
 
 static int pmicmpfnc(const void *_a, const void *_b)
@@ -326,7 +321,7 @@ static int pmicmpfnc(const void *_a, const void *_b)
 	const playermodelinfo_s *a = (const playermodelinfo_s *) _a;
 	const playermodelinfo_s *b = (const playermodelinfo_s *) _b;
 
-	// Sort by male, female, then alphabetical
+	// Sort by male, female, cyborg then alphabetical
 	if (strcmp(a->directory, "male") == 0)
 		return -1;
 	if (strcmp(b->directory, "male") == 0)
@@ -335,6 +330,11 @@ static int pmicmpfnc(const void *_a, const void *_b)
 	if (strcmp(a->directory, "female") == 0)
 		return -1;
 	if (strcmp(b->directory, "female") == 0)
+		return 1;
+
+	if (strcmp(a->directory, "cyborg") == 0) //mxd
+		return -1;
+	if (strcmp(b->directory, "cyborg") == 0)
 		return 1;
 
 	return strcmp(a->directory, b->directory);