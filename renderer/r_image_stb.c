a728323a220cf2e1b6160d12787dab006cd20d7a
m-x-d <j.maxed@gmail.com>
2019-05-21 05:50:23 +0300

    Renamed snd_stream.c to snd_ogg.c. Renamed include/stb to include/stb_image.

--- renderer/r_image_stb.c
+++ renderer/r_image_stb.c
@@ -44,15 +44,15 @@
 
 // Include implementation part of stb_image into this file
 #define STB_IMAGE_IMPLEMENTATION
-#include "../include/stb/stb_image.h"
+#include "../include/stb_image/stb_image.h"
 
 // Include resize implementation
 #define STB_IMAGE_RESIZE_IMPLEMENTATION
-#include "../include/stb/stb_image_resize.h"
+#include "../include/stb_image/stb_image_resize.h"
 
 // Include write implementation
 #define STB_IMAGE_WRITE_IMPLEMENTATION
-#include "../include/stb/stb_image_write.h"
+#include "../include/stb_image/stb_image_write.h"
 
 /*
  * origname: the filename to be opened, might be without extension
