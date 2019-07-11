5621e2e5854286f83c106b514adbfb947713dc0e
MaxED <j.maxed@gmail.com>
2019-07-11 16:36:26 +0300

    Renamed snd_dma.c to snd_main.c. Renamed snd_mem.c to snd_wav.c. Renamed glw_imp.c to glimp_sdl2.c. Renamed in_win.c to cl_input_sdl2.c, moved to "client" folder.

--- client/cl_input_sdl2.c
+++ client/cl_input_sdl2.c
@@ -17,9 +17,9 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 */
-// in_sdl2.c -- SDL2 mouse and gamepad code
+// cl_input_sdl2.c -- SDL2 mouse and gamepad code
 
-#include "../client/client.h"
+#include "client.h"
 #include "../win32/sdlquake.h" //mxd
 
 // The last time input events were processed. Used throughout the client.
